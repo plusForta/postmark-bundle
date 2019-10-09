5933e1bcfa3fc7e3efa9b93a3c4859b62ba38066
Jan Hentschel <jan.hentschel@plusforta.de>
2019-10-09 14:58:30 +0200

    Refs PF-5867: Ruby zu PHP - Crons - Tests für Transaction Mail Service (Postmark)
    Default logger is NullLogger but it is now replaced by logger, if it is
    set in project.

--- src/PlusFortaPostmarkBundle.php
+++ src/PlusFortaPostmarkBundle.php
@@ -2,11 +2,17 @@
 
 namespace PlusForta\PostmarkBundle;
 
+use PlusForta\PostmarkBundle\DependencyInjection\Compiler\PostmarkBundleCompilerPass;
 use PlusForta\PostmarkBundle\DependencyInjection\PlusFortaPostmarkExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PlusFortaPostmarkBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new PostmarkBundleCompilerPass());
+    }
 
     /**
      * Overridden to allow for the custom extension alias.
