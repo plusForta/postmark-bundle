5b944b1f1816f7b48a0c32a1eb561ef5089fe05e
Anar Hagverdiyev <anarh@Anar-Hagverdiyev---Plusforta-Macbook-Pro.local>
2023-09-28 12:57:23 +0200

    Add return value

--- src/PlusFortaPostmarkBundle.php
+++ src/PlusFortaPostmarkBundle.php
@@ -5,6 +5,7 @@ namespace PlusForta\PostmarkBundle;
 use PlusForta\PostmarkBundle\DependencyInjection\Compiler\PostmarkBundleCompilerPass;
 use PlusForta\PostmarkBundle\DependencyInjection\PlusFortaPostmarkExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PlusFortaPostmarkBundle extends Bundle
@@ -17,11 +18,12 @@ class PlusFortaPostmarkBundle extends Bundle
     /**
      * Overridden to allow for the custom extension alias.
      */
-    public function getContainerExtension()
+    public function getContainerExtension(): ExtensionInterface
     {
         if (null === $this->extension) {
             $this->extension = new PlusFortaPostmarkExtension();
         }
+
         return $this->extension;
     }
 
